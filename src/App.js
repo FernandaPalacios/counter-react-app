d6dff96e2e2a993059d277233af7a919ffb9a7dc
Fernanda Palacios <fernandapalacios@Fernandas-MBP.localdomain>
2019-05-07 16:57:18 -0400

    add app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class CounterApp extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {count : 0}
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment(){
+    this.setState({count: this.state.count + 1});
+  }
+
+  decrement(){
+    this.setState({count: this.state.count - 1});
+  }
+
+  render(){
+  
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Counter: {this.state.count}</p>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+
       </header>
     </div>
   );
+  }
 }
 
-export default App;
+export default CounterApp;
